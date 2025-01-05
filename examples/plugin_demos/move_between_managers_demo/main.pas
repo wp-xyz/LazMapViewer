77ed94f6f258d68b03e29aa745999d62944430b1
wp_xyz <wp_xxyyzzz@gmx.net>
2025-01-05 18:24:34 +0100

    Add demo which moves a plugin between two plugin managers.

---
+++ examples/plugin_demos/move_between_managers_demo/main.pas
@@ -0,0 +1,52 @@
+unit main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, ExtCtrls, mvMapViewer, mvPluginCore, mvPlugins, StdCtrls, SysUtils,
+  Forms, Controls, Graphics, Dialogs;
+
+type
+  TForm1 = class(TForm)
+    CenterBevel: TBevel;
+    btnMove: TButton;
+    MapView_Left: TMapView;
+    MapView_Right: TMapView;
+    MvPluginManager_Left: TMvPluginManager;
+    CenterMarkerPlugin: TCenterMarkerPlugin;
+    MvPluginManager_Right: TMvPluginManager;
+    procedure btnMoveClick(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+procedure TForm1.btnMoveClick(Sender: TObject);
+begin
+  if CenterMarkerPlugin.PluginManager = MvPluginManager_Left then
+    CenterMarkerPlugin.PluginManager := MvPluginManager_Right
+  else
+    CenterMarkerPlugin.PluginManager := MvPluginManager_Left;
+  MapView_Left.Invalidate;
+  MapView_Right.Invalidate;
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  MapView_Left.Active := true;
+  MapView_Right.Active := true;
+end;
+
+end.
+
